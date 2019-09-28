e6903510e3c2294e57a5fe5f93971d3e5564eb3f
Arthur Zherko <arthur@maxiru.com>
2019-09-28 14:11:25 +0300

    added dotenv

--- config/default.js
+++ config/default.js
@@ -1,22 +1,23 @@
 const defer = require('config/defer').deferConfig;
 const path = require('path');
+require('dotenv').config()
 
 module.exports = {
   server: {
-    siteHost: 'http://localhost'
+    siteHost: process.env.SITE_HOST
   },
   mailer: {
-    transport: 'gmail',
+    transport: process.env.MAIL_TRANSPORT,
     gmail: {
-      user: 'arthurzherko@gmail.com',
-      password: 'asdsdasd'
+      user: process.env.MAIL_USER,
+      password: process.env.MAIL_PASSWORD
     },
     senders:  {
       // transactional emails, register/forgot pass etc
       default:  {
-        fromEmail: 'arthurzherko@gmail.com',
-        fromName:  'Arthur Zherka',
-        signature: "<em>С уважением,<br>Arthur</em>"
+        fromEmail: process.env.MAIL_FROM,
+        fromName:  process.env.MAIL_FROM_NAME,
+        signature: process.env.MAIL_SIGNATURE
       },
       /* newsletters example
       informer: {
@@ -28,7 +29,7 @@ module.exports = {
     }
   },
   mongoose: {
-    uri:     'mongodb://localhost/mailer',
+    uri:     process.env.MAIL_SIGNATURE.MONGOOSE_URI,
     options: {
       useNewUrlParser: true,
       useCreateIndex: true,
